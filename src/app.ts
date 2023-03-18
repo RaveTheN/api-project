590cf93ac07468071a5150884bf3f5d2e5d02a0e
RaveTheN <peppereres@live.it>
2023-03-18 23:45:58 +0100

    package bug fixed

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,18 @@ import "express-async-errors";
 
 const app = express();
 
+function getPlanets() {
+  const planets = [{ name: "Mercury" }, { name: "Venus" }, { name: "Earth" }];
+
+  //planets.push({ name: "Mars" });
+
+  return planets;
+}
+
 app.get("/planets", (request, response) => {
-  response.json([{ name: "Mercury" }, { name: "Venus" }]);
+  const planets = getPlanets();
+
+  response.json(planets);
 });
 
 export default app;
