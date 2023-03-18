2c46eb4159b24628a92c43af7d67651238fcc23b
RaveTheN <peppereres@live.it>
2023-03-18 16:45:39 +0100

    add http server with express

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
-const name: string = "Peppe";
+import express from "express";
+import "express-async-errors";
 
-console.log(`Hello ${name}`);
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Up and running!");
+});
+
+const port = 3000;
+
+app.listen(port, () =>
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+);
